aa7735f877921489c09f7dce5311d47518b045f2
rachitdhar <75770595+rachitdhar@users.noreply.github.com>
2025-02-04 13:13:06 +0530

    committing files

---
+++ Form1.Designer.vb
@@ -0,0 +1,213 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class WinApp
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        AddButton = New Button()
+        ProjectDropdown = New ComboBox()
+        Label2 = New Label()
+        EnvDropdown = New ComboBox()
+        MainPathLabel = New Label()
+        MainLocationTextBox = New TextBox()
+        FolderBrowserDialog1 = New FolderBrowserDialog()
+        BrowseButton = New Button()
+        AdditionalSettingsGroupBox = New GroupBox()
+        LoadFunctionsButton = New Button()
+        FunctionsComboBox = New ComboBox()
+        Label3 = New Label()
+        CheckBoxLocalSettings = New CheckBox()
+        BottomPanel = New Panel()
+        AdditionalSettingsGroupBox.SuspendLayout()
+        BottomPanel.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(31, 23)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(44, 15)
+        Label1.TabIndex = 0
+        Label1.Text = "Project"
+        ' 
+        ' AddButton
+        ' 
+        AddButton.Location = New Point(97, 62)
+        AddButton.Name = "AddButton"
+        AddButton.Size = New Size(80, 23)
+        AddButton.TabIndex = 2
+        AddButton.Text = "Add"
+        AddButton.UseVisualStyleBackColor = True
+        ' 
+        ' ProjectDropdown
+        ' 
+        ProjectDropdown.FormattingEnabled = True
+        ProjectDropdown.Items.AddRange(New Object() {"ExampleProj"})
+        ProjectDropdown.Location = New Point(164, 20)
+        ProjectDropdown.Name = "ProjectDropdown"
+        ProjectDropdown.Size = New Size(137, 23)
+        ProjectDropdown.TabIndex = 3
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(31, 51)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(75, 15)
+        Label2.TabIndex = 4
+        Label2.Text = "Environment"
+        ' 
+        ' EnvDropdown
+        ' 
+        EnvDropdown.FormattingEnabled = True
+        EnvDropdown.Items.AddRange(New Object() {"ExampleEnv"})
+        EnvDropdown.Location = New Point(164, 48)
+        EnvDropdown.Name = "EnvDropdown"
+        EnvDropdown.Size = New Size(137, 23)
+        EnvDropdown.TabIndex = 5
+        ' 
+        ' MainPathLabel
+        ' 
+        MainPathLabel.AutoSize = True
+        MainPathLabel.Location = New Point(0, 9)
+        MainPathLabel.Name = "MainPathLabel"
+        MainPathLabel.Size = New Size(70, 15)
+        MainPathLabel.TabIndex = 6
+        MainPathLabel.Text = "Location of "
+        ' 
+        ' MainLocationTextBox
+        ' 
+        MainLocationTextBox.Location = New Point(0, 27)
+        MainLocationTextBox.Name = "MainLocationTextBox"
+        MainLocationTextBox.Size = New Size(197, 23)
+        MainLocationTextBox.TabIndex = 7
+        ' 
+        ' BrowseButton
+        ' 
+        BrowseButton.Location = New Point(203, 27)
+        BrowseButton.Name = "BrowseButton"
+        BrowseButton.Size = New Size(70, 23)
+        BrowseButton.TabIndex = 8
+        BrowseButton.Text = "Browse"
+        BrowseButton.UseVisualStyleBackColor = True
+        ' 
+        ' AdditionalSettingsGroupBox
+        ' 
+        AdditionalSettingsGroupBox.Controls.Add(LoadFunctionsButton)
+        AdditionalSettingsGroupBox.Controls.Add(FunctionsComboBox)
+        AdditionalSettingsGroupBox.Controls.Add(Label3)
+        AdditionalSettingsGroupBox.Controls.Add(CheckBoxLocalSettings)
+        AdditionalSettingsGroupBox.Location = New Point(31, 87)
+        AdditionalSettingsGroupBox.Name = "AdditionalSettingsGroupBox"
+        AdditionalSettingsGroupBox.Size = New Size(273, 128)
+        AdditionalSettingsGroupBox.TabIndex = 9
+        AdditionalSettingsGroupBox.TabStop = False
+        AdditionalSettingsGroupBox.Text = "Additional Settings"
+        AdditionalSettingsGroupBox.Visible = False
+        ' 
+        ' LoadFunctionsButton
+        ' 
+        LoadFunctionsButton.Location = New Point(203, 82)
+        LoadFunctionsButton.Name = "LoadFunctionsButton"
+        LoadFunctionsButton.Size = New Size(53, 23)
+        LoadFunctionsButton.TabIndex = 3
+        LoadFunctionsButton.Text = "Load"
+        LoadFunctionsButton.UseVisualStyleBackColor = True
+        ' 
+        ' FunctionsComboBox
+        ' 
+        FunctionsComboBox.Enabled = False
+        FunctionsComboBox.FormattingEnabled = True
+        FunctionsComboBox.Location = New Point(18, 82)
+        FunctionsComboBox.Name = "FunctionsComboBox"
+        FunctionsComboBox.Size = New Size(179, 23)
+        FunctionsComboBox.TabIndex = 2
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(16, 64)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(123, 15)
+        Label3.TabIndex = 1
+        Label3.Text = "Function for host.json"
+        ' 
+        ' CheckBoxLocalSettings
+        ' 
+        CheckBoxLocalSettings.AutoSize = True
+        CheckBoxLocalSettings.Location = New Point(18, 30)
+        CheckBoxLocalSettings.Name = "CheckBoxLocalSettings"
+        CheckBoxLocalSettings.Size = New Size(139, 19)
+        CheckBoxLocalSettings.TabIndex = 0
+        CheckBoxLocalSettings.Text = "Set local.settings.json"
+        CheckBoxLocalSettings.UseVisualStyleBackColor = True
+        ' 
+        ' BottomPanel
+        ' 
+        BottomPanel.Controls.Add(MainLocationTextBox)
+        BottomPanel.Controls.Add(MainPathLabel)
+        BottomPanel.Controls.Add(BrowseButton)
+        BottomPanel.Controls.Add(AddButton)
+        BottomPanel.Location = New Point(31, 221)
+        BottomPanel.Name = "BottomPanel"
+        BottomPanel.Size = New Size(273, 85)
+        BottomPanel.TabIndex = 10
+        ' 
+        ' WinApp
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(335, 319)
+        Controls.Add(BottomPanel)
+        Controls.Add(AdditionalSettingsGroupBox)
+        Controls.Add(EnvDropdown)
+        Controls.Add(Label2)
+        Controls.Add(ProjectDropdown)
+        Controls.Add(Label1)
+        Name = "WinApp"
+        Text = "Add Appsettings"
+        AdditionalSettingsGroupBox.ResumeLayout(False)
+        AdditionalSettingsGroupBox.PerformLayout()
+        BottomPanel.ResumeLayout(False)
+        BottomPanel.PerformLayout()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents AddButton As Button
+    Friend WithEvents ProjectDropdown As ComboBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents EnvDropdown As ComboBox
+    Friend WithEvents MainPathLabel As Label
+    Friend WithEvents MainLocationTextBox As TextBox
+    Friend WithEvents FolderBrowserDialog1 As FolderBrowserDialog
+    Friend WithEvents BrowseButton As Button
+    Friend WithEvents AdditionalSettingsGroupBox As GroupBox
+    Friend WithEvents CheckBoxLocalSettings As CheckBox
+    Friend WithEvents FunctionsComboBox As ComboBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents BottomPanel As Panel
+    Friend WithEvents LoadFunctionsButton As Button
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,166 @@
+﻿Imports System.IO
+Imports System.Text
+Imports System.Text.Json
+Imports System.Text.Json.Nodes
+Imports System.Text.RegularExpressions
+
+Public Class WinApp
+    Dim DefaultMainPath As String
+    Dim AppRoot As String
+    Dim paths As JsonObject
+    Dim ProjectsWithAdditionalSettings As List(Of String)
+
+    Private Sub AddButton_Click(sender As Object, e As EventArgs) Handles AddButton.Click
+        Dim projectSelected As String = ProjectDropdown.Text
+        Dim envSelected As String = EnvDropdown.Text
+        Dim location As String = MainLocationTextBox.Text.Trim()
+
+        If (location.ElementAt(location.Length - 1) = "\" Or location.ElementAt(location.Length - 1) = "/") Then
+            location = location.Remove(location.Length - 1)
+        End If
+        location += paths(projectSelected).ToString()
+
+        Dim appSettingsLocation As String = location + "appsettings.json"
+        Dim localLocation As String = location + "local.settings.json"
+        Dim hostLocation As String = location + "host.json"
+        Dim envSettingsLocation = AppRoot + "\appsettings\" + envSelected + ".json"
+        Dim envSettings As JsonObject = JsonObject.Parse(File.ReadAllText(envSettingsLocation))
+
+        Try
+            'Add Appsettings
+            Dim jsonData As String = File.ReadAllText(appSettingsLocation)
+            Dim jObject As JsonObject = JsonObject.Parse(jsonData)
+
+            If (jObject("ConnectionString") IsNot Nothing) And (jObject("Serilog") IsNot Nothing) Then
+                jObject("ConnectionString") = envSettings("ConnectionString").DeepClone()
+
+                Dim SerilogJObj As JsonObject = CType(jObject("Serilog"), JsonObject)
+                If SerilogJObj("WriteTo") IsNot Nothing Then
+                    SerilogJObj("WriteTo") = envSettings("WriteTo").DeepClone()
+                End If
+            Else
+                Throw New Exception()
+            End If
+
+            Dim options As New JsonSerializerOptions With {
+                .Encoder = Encodings.Web.JavaScriptEncoder.UnsafeRelaxedJsonEscaping,
+                .WriteIndented = True
+            }
+            Dim jsonString As String = JsonSerializer.Serialize(jObject, options)
+            File.WriteAllText(appSettingsLocation, jsonString, Encoding.UTF8)
+
+            If (ProjectsWithAdditionalSettings.Contains(projectSelected)) Then
+                'Add Local Settings
+                If (CheckBoxLocalSettings.Checked) Then
+                    Dim localJObject As JsonObject = JsonObject.Parse(File.ReadAllText(localLocation))
+
+                    If (localJObject("Values") IsNot Nothing) Then
+                        Dim ValuesJObj As JsonObject = CType(localJObject("Values"), JsonObject)
+                        If ValuesJObj("ServiceBusConnection") IsNot Nothing Then
+                            Dim ConnectionStringJObj As JsonObject = CType(envSettings("ConnectionString"), JsonObject)
+                            ValuesJObj("ServiceBusConnection") = ConnectionStringJObj("ServiceBusConnection").DeepClone()
+                        End If
+                    End If
+
+                    Dim localJsonString As String = JsonSerializer.Serialize(localJObject, options)
+                    File.WriteAllText(localLocation, localJsonString, Encoding.UTF8)
+                End If
+
+                'Add Host Function
+                If (FunctionsComboBox.Text IsNot "") And (FunctionsComboBox.Text IsNot "Select Function") Then
+                    Dim hostJsonContent As String = File.ReadAllText(hostLocation)
+                    Dim commentedPattern As String = "//\s*,\s*""functions""\s*:\s*\[.*?\]"
+
+                    ' Remove the comment on functions if it exists
+                    If Regex.IsMatch(hostJsonContent, commentedPattern) Then
+                        hostJsonContent = Regex.Replace(hostJsonContent, commentedPattern, ",""functions"": []")
+                    End If
+
+                    Dim hostJObject As JsonObject = JsonObject.Parse(hostJsonContent)
+                    hostJObject("functions") = New JsonArray(FunctionsComboBox.Text)
+
+                    Dim hostJsonString As String = JsonSerializer.Serialize(hostJObject, options)
+                    File.WriteAllText(hostLocation, hostJsonString, Encoding.UTF8)
+                End If
+            End If
+            MsgBox("Success")
+        Catch ex As Exception
+            MsgBox("Failed to set appsettings")
+        End Try
+    End Sub
+
+    Private Sub WinApp_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        BottomPanel.Top = EnvDropdown.Bottom + 10
+        Me.Height = BottomPanel.Bottom + 50
+        AppRoot = GetParentDirectory(AppDomain.CurrentDomain.BaseDirectory, 4)
+        Dim pathsFileLocation As String = AppRoot + "\paths.json"
+        paths = JsonObject.Parse(File.ReadAllText(pathsFileLocation))
+        DefaultMainPath = paths("DefaultMainPath").ToString()
+        ProjectsWithAdditionalSettings = Newtonsoft.Json.JsonConvert.DeserializeObject(Of List(Of String))(JsonSerializer.Serialize(paths("ProjectsWithAdditionalSettings")))
+
+        ProjectDropdown.SelectedIndex = 0
+        EnvDropdown.SelectedIndex = 0
+        MainLocationTextBox.Text = DefaultMainPath
+        MainPathLabel.Text = "Location of " + DefaultMainPath.Split("\")(1)
+    End Sub
+
+    Private Function GetParentDirectory(path As String, levelsUp As Integer) As String
+        Dim currentPath As String = path
+        For i As Integer = 1 To levelsUp
+            currentPath = Directory.GetParent(currentPath).FullName
+        Next
+        Return currentPath
+    End Function
+
+    Private Sub BrowseButton_Click(sender As Object, e As EventArgs) Handles BrowseButton.Click
+        If (FolderBrowserDialog1.ShowDialog = DialogResult.OK) Then
+            MainLocationTextBox.Text = FolderBrowserDialog1.SelectedPath
+        End If
+    End Sub
+
+    Private Sub ProjectDropdown_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ProjectDropdown.SelectedIndexChanged
+        If (ProjectsWithAdditionalSettings.Contains(ProjectDropdown.Text)) Then
+            AdditionalSettingsGroupBox.Visible = True
+            BottomPanel.Top = AdditionalSettingsGroupBox.Bottom + 10
+            Me.Height = BottomPanel.Bottom + 50
+        Else
+            AdditionalSettingsGroupBox.Visible = False
+            CheckBoxLocalSettings.Checked = False
+            BottomPanel.Top = EnvDropdown.Bottom + 10
+            Me.Height = BottomPanel.Bottom + 50
+        End If
+    End Sub
+
+    Private Sub LoadFunctionsList()
+        Dim pattern As String = "\[Function\(nameof\((?<name>[a-zA-Z_][a-zA-Z0-9_]*)\)\)\]"
+        Dim matchesList As New List(Of String)
+        Dim allFiles = Directory.GetFiles(DefaultMainPath, "*.cs", SearchOption.AllDirectories)
+
+        ' Process each file
+        For Each f In allFiles
+            Dim content As String = File.ReadAllText(f)
+            Dim matches = Regex.Matches(content, pattern)
+
+            For Each match As Match In matches
+                matchesList.Add(match.Groups("name").Value)
+            Next
+        Next
+        FunctionsComboBox.Items.Clear()
+        FunctionsComboBox.Items.AddRange(matchesList.ToArray)
+        FunctionsComboBox.Sorted = True
+    End Sub
+
+    Private Sub LoadFunctionsButton_Click(sender As Object, e As EventArgs) Handles LoadFunctionsButton.Click
+        FunctionsComboBox.Enabled = False
+        FunctionsComboBox.Text = "Loading..."
+        Try
+            LoadFunctionsList()
+            FunctionsComboBox.Text = "Select Function"
+            FunctionsComboBox.Enabled = True
+        Catch ex As Exception
+            FunctionsComboBox.Items.Clear()
+            FunctionsComboBox.Text = ""
+            MsgBox("Failed to load the functions")
+        End Try
+    End Sub
+End Class
